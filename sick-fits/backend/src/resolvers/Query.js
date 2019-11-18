3a1d8037f5bdb6869ca8ea302f36dd0331b8a03b
benjamin-otto <benjaminotto@protonmail.ch>
2019-11-18 18:04:09 +0100

    Added Item query and mutation.

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,13 @@
-const Query = {}
+const { forwardTo } = require('prisma-binding')
+
+const Query = {
+  // If the query is exactly how Prisma has defined it we can just forward it
+  items: forwardTo('db')
+  // This does the same as the above line...
+  // async items(parent, args, ctx, info) {
+  //   const items = await ctx.db.query.items()
+  //   return items
+  // }
+}
 
 module.exports = Query
